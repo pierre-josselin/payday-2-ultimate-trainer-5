2d6066b8ba4058abc85ef39e657a6f23771a7262
Pierre Josselin <contact@pierre-josselin.fr>
2022-02-18 13:23:52 +0100

    Add alerts

--- classes/UT.lua
+++ classes/UT.lua
@@ -5,6 +5,13 @@ UT.settings = {}
 UT.saveFilesNames = {}
 UT.saveFilesNames.settings = "ut-settings.json"
 
+UT.colors = {
+    white = Color("ffffff"),
+    success = Color("00ff00"),
+    warning = Color("ffff00"),
+    danger = Color("ff0000")
+}
+
 function UT:loadSettings()
     UT.settings = UT.Utils:getSaveTable(UT.saveFilesNames.settings)
     return true
@@ -16,4 +23,15 @@ end
 
 function UT:log(data)
     log(UT.Utils:toString(data))
+end
+
+function UT:getLocalizedText(stringId)
+    return managers.localization:text(stringId)
+end
+
+function UT:addAlert(message, color, localized)
+    if localized or localized == nil then
+        message = UT:getLocalizedText(message)
+    end
+    managers.mission._fading_debug_output:script().log(message, color or UT.colors.white)
 end
--- hooks/lib/managers/menumanager.lua
+++ hooks/lib/managers/menumanager.lua
@@ -10,16 +10,6 @@ Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_UltimateTr
 end)
 
 Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", function(menuManager)
-    MenuCallbackHandler.ut_toggle_dlc_unlocker = function(self, item)
-        local value = UT.Utils:getToggleValue(item:value())
-        UT.Unlocker:toggleDlcUnlocker(value)
-    end
-
-    MenuCallbackHandler.ut_toggle_skin_unlocker = function(self, item)
-        local value = UT.Utils:getToggleValue(item:value())
-        UT.Unlocker:toggleSkinUnlocker(value)
-    end
-
     MenuCallbackHandler.ut_set_level = function(self, item)
         local level = item:value()
         item:set_value("")
@@ -34,6 +24,7 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:setLevel(level)
+        UT:addAlert("ut_alert_level_set", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_set_infamy_rank = function(self, item)
@@ -50,6 +41,7 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:setInfamyRank(infamyRank)
+        UT:addAlert("ut_alert_infamy_rank_set", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_spending_money = function(self, item)
@@ -66,6 +58,7 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:addSpendingMoney(amount)
+        UT:addAlert("ut_alert_spending_money_added", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_offshore_money = function(self, item)
@@ -82,10 +75,12 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:addOffshoreMoney(amount)
+        UT:addAlert("ut_alert_offshore_money_added", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_reset_money = function(self, item)
         UT.Player:resetMoney()
+        UT:addAlert("ut_alert_money_reset", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_continental_coins = function(self, item)
@@ -102,15 +97,22 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:addContinentalCoins(amount)
+        UT:addAlert("ut_alert_continental_coins_added", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_reset_continental_coins = function(self, item)
         UT.Player:resetContinentalCoins()
+        UT:addAlert("ut_alert_continental_coins_reset", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_toggle_skill_points_hack = function(self, item)
         local value = UT.Utils:getToggleValue(item:value())
         UT.Player:toggleSkillPointsHack(value)
+        if value then
+            UT:addAlert("ut_alert_skill_points_hack_enabled", UT.colors.success)
+        else
+            UT:addAlert("ut_alert_skill_points_hack_disabled", UT.colors.success)
+        end
     end
 
     MenuCallbackHandler.ut_set_skill_points_total_amount = function(self, item)
@@ -126,6 +128,7 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:setSkillPointsTotalAmount(amount)
+        UT:addAlert("ut_alert_skill_points_total_amount_set", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_perk_experience = function(self, item)
@@ -142,94 +145,137 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_UltimateTrainer", func
             return
         end
         UT.Player:addPerkPoints(amount)
+        UT:addAlert("ut_alert_perk_experience_added", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_reset_perk_points = function(self, item)
         UT.Player:resetPerkPoints()
+        UT:addAlert("ut_alert_perk_points_reset", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_one_of_all_weapon_mods = function(self, item)
         UT.Player:addItemsToInventory("weapon_mods")
+        UT:addAlert("ut_alert_added_one_of_all_weapon_mods", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_one_of_all_masks = function(self, item)
         UT.Player:addItemsToInventory("masks")
+        UT:addAlert("ut_alert_added_one_of_all_masks", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_one_of_all_materials = function(self, item)
         UT.Player:addItemsToInventory("materials")
+        UT:addAlert("ut_alert_added_one_of_all_materials", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_one_of_all_patterns = function(self, item)
         UT.Player:addItemsToInventory("textures")
+        UT:addAlert("ut_alert_added_one_of_all_patterns", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_add_one_of_all_colors = function(self, item)
         UT.Player:addItemsToInventory("colors")
+        UT:addAlert("ut_alert_added_one_of_all_colors", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_clear_weapon_mods = function(self, item)
         UT.Player:clearInventoryItems("weapon_mods")
+        UT:addAlert("ut_alert_weapon_mods_cleared", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_clear_masks = function(self, item)
         UT.Player:clearInventoryItems("masks")
+        UT:addAlert("ut_alert_masks_cleared", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_clear_materials = function(self, item)
         UT.Player:clearInventoryItems("materials")
+        UT:addAlert("ut_alert_materials_cleared", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_clear_patterns = function(self, item)
         UT.Player:clearInventoryItems("textures")
+        UT:addAlert("ut_alert_patterns_cleared", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_clear_colors = function(self, item)
         UT.Player:clearInventoryItems("colors")
+        UT:addAlert("ut_alert_colors_cleared", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_weapons = function(self, item)
         UT.Player:unlockInventoryCategory("weapon")
+        UT:addAlert("ut_alert_unlocked_all_weapons", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_melee_weapons = function(self, item)
         UT.Player:unlockInventoryCategory("melee_weapon")
+        UT:addAlert("ut_alert_unlocked_all_melee_weapons", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_throwables = function(self, item)
         UT.Player:unlockInventoryCategory("grenade")
+        UT:addAlert("ut_alert_unlocked_all_throwables", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_armors = function(self, item)
         UT.Player:unlockInventoryCategory("armor")
+        UT:addAlert("ut_alert_unlocked_all_armors", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_remove_exclamation_marks = function(self, item)
         UT.Player:removeExclamationMarks()
+        UT:addAlert("ut_alert_exclamation_marks_removed", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_slots = function(self, item)
         UT.Player:setAllSlots(true)
+        UT:addAlert("ut_alert_unlocked_all_slots", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_lock_all_slots = function(self, item)
         UT.Player:setAllSlots(false)
+        UT:addAlert("ut_alert_locked_all_slots", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_trophies = function(self, item)
         UT.Player:unlockAllTrophies()
+        UT:addAlert("ut_alert_unlocked_all_trophies", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_lock_all_trophies = function(self, item)
         UT.Player:lockAllTrophies()
+        UT:addAlert("ut_alert_locked_all_trophies", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_unlock_all_steam_achievements = function(self, item)
         UT.Player:unlockAllSteamAchievements()
+        UT:addAlert("ut_alert_unlocked_all_steam_achievements", UT.colors.success)
     end
 
     MenuCallbackHandler.ut_lock_all_steam_achievements = function(self, item)
         UT.Player:lockAllSteamAchievements()
+        UT:addAlert("ut_alert_locked_all_steam_achievements", UT.colors.success)
+    end
+
+    MenuCallbackHandler.ut_toggle_dlc_unlocker = function(self, item)
+        local value = UT.Utils:getToggleValue(item:value())
+        UT.Unlocker:toggleDlcUnlocker(value)
+        if value then
+            UT:addAlert("ut_alert_dlc_unlocker_enabled", UT.colors.success)
+        else
+            UT:addAlert("ut_alert_dlc_unlocker_disabled", UT.colors.success)
+        end
+    end
+
+    MenuCallbackHandler.ut_toggle_skin_unlocker = function(self, item)
+        local value = UT.Utils:getToggleValue(item:value())
+        UT.Unlocker:toggleSkinUnlocker(value)
+        if value then
+            UT:addAlert("ut_alert_skin_unlocker_enabled", UT.colors.success)
+        else
+            UT:addAlert("ut_alert_skin_unlocker_disabled", UT.colors.success)
+        end
     end
 
     MenuHelper:LoadFromJsonFile(modPath .. "menus/main.json", nil, UT.settings)
