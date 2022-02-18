ed156002254beab9adc2a6b531530dee8ad64b25
Pierre Josselin <contact@pierre-josselin.fr>
2022-02-18 14:45:53 +0100

    Improve skin unlocker

--- classes/Unlocker.lua
+++ classes/Unlocker.lua
@@ -8,6 +8,17 @@ end
 function UT.Unlocker:toggleSkinUnlocker(value)
     UT.settings.enableSkinUnlocker = value
     UT:saveSettings()
+    if value then
+        for skinName, skinData in pairs(tweak_data.blackmarket.weapon_skins) do
+            local instanceId = #managers.blackmarket._global.inventory_tradable + 1
+            if not managers.blackmarket:have_inventory_tradable_item("weapon_skins", skinName) and not skinData.is_a_color_skin then
+                managers.blackmarket:tradable_add_item(instanceId, "weapon_skins", skinName, "mint", false, 1)
+            end
+        end
+    else
+        managers.blackmarket._global.inventory_tradable = {}
+    end
+    UT.Player:refreshAndSave()
 end
 
 UTLoadedClassUnlocker = true
