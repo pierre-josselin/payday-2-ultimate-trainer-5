759d983e3917d902dea4b02f2ce9895098a3b241
Pierre Josselin <contact@pierre-josselin.fr>
2022-03-15 23:44:27 +0100

    Update version number

--- classes/UT.lua
+++ classes/UT.lua
@@ -1,6 +1,6 @@
 UT = {}
 
-UT.version = "5.1.0-beta"
+UT.version = "5.2.0"
 UT.threadUrl = "https://www.unknowncheats.me/forum/payday-2-a/491561-payday-2-ultimate-trainer-5-a.html"
 
 UT.supportedLocales = {
