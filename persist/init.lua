02b525f2f3d020ffd21598d02ea465f392e05923
Pierre Josselin <contact@pierre-josselin.fr>
2022-03-15 16:25:22 +0100

    Remove unnecessary fading debug output configuration

--- persist/init.lua
+++ persist/init.lua
@@ -2,9 +2,4 @@ UT:loadSettings()
 
 UT.debugLogClass = DebugLogClass:new()
 
-managers.mission._fading_debug_output:script().configure({
-    font_size = 22,
-    max_rows = 10
-})
-
 UTLoadedPersistInit = true
