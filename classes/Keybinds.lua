6bd562cac991fcfc1aa5332cfb2947a704a15fa8
Pierre Josselin <contact@pierre-josselin.fr>
2022-03-18 21:36:31 +0100

    Remove throwables from replenish keybind
    Because it is detected by the anti cheat

--- classes/Keybinds.lua
+++ classes/Keybinds.lua
@@ -21,7 +21,7 @@ function UT.Keybinds:replenish()
         return
     end
 
-    managers.player:add_grenade_amount(99)
+    --managers.player:add_grenade_amount(99)
     managers.player:add_body_bags_amount(99)
     managers.player:add_special({name = "cable_tie", amount = 99})
     managers.player:set_player_state("standard")
